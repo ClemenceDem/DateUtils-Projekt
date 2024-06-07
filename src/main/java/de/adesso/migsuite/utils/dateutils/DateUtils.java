be36ca27241557ef19b3ae3c1bb64c1752b0fdba
Clemence Demanou <clemence.demanou@adesso-insurance-solutions.de>
2024-06-07 14:38:49 +0200

    Methode localDateToLongs

--- src/main/java/de/adesso/migsuite/utils/dateutils/DateUtils.java
+++ src/main/java/de/adesso/migsuite/utils/dateutils/DateUtils.java
@@ -1,16 +1,9 @@
 package de.adesso.migsuite.utils.dateutils;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class DateUtils {
-
-    private static final Date Date = null;
-    private static final String format = null;
-    final SimpleDateFormat SIMPLE_DATE_FORMAT_YYYY_DEFAULT = new SimpleDateFormat("yyyy-MM-dd");
-
 
     public static LocalDate localDateFromStrings(String day, String month, String year) {
 
@@ -22,16 +15,12 @@ public class DateUtils {
 
     public static LocalDate localDateFromIntegers(int day, int month, int year) {
 
-        LocalDate date = LocalDate.of(year, month, day);
-
-        return date;
+        return LocalDate.of(year, month, day);
     }
 
     public static LocalDate localDateFromLongs(long day, long month, long year) {
 
-        LocalDate date = LocalDate.of((int) year, (int) month, (int) day);
-
-        return date;
+        return LocalDate.of((int) year, (int) month, (int) day);
     }
 
     public static LocalDate stringsToLocalDate(String day, String month, String year) {
@@ -65,15 +54,13 @@ public class DateUtils {
 
     }
 
-    public static String localDateToLongs(long day, long month, long year) {
+    public static String localDateToLongs(long day, long month, long year, String format) {
+        {
 
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(format);
-        return localDateFromLongs((int) day, (int) month, (int) year).format(outputFormatter);
+            DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(format);
+            return localDateFromLongs((int) day, (int) month, (int) year).format(outputFormatter);
+
+        }
 
     }
-
-    /*
-     * public static void main(String[] args) { System.out.println(dateFromLongs(06, 06, 2024)); }
-     */
-
 }
