f3842350c8321eb1c789acd8704ae929f56936ee
Clemence Demanou <clemence.demanou@adesso-insurance-solutions.de>
2024-06-14 16:09:49 +0200

    B2,3,4

--- src/main/java/de/adesso/migsuite/utils/dateutils/DateUtils.java
+++ src/main/java/de/adesso/migsuite/utils/dateutils/DateUtils.java
@@ -1,11 +1,22 @@
 package de.adesso.migsuite.utils.dateutils;
 
-
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-//  Methoden zur Manipulation von Datumsangaben.
+/**
+ * Die Klasse DateUtils bietet Hilfsmethoden zur Umwandlung von Datumsangaben in verschiedene Date-Objekte von org.joda.time und java.time.
+ * 
+ * Diese Klasse enthält Methoden zur Erstellung von LocalDate-Objekten aus übergebenen Strings oder numerischen Werten sowie zur Formatierung und
+ * Umwandlung von Datumsstrings in spezifizierte Formate.
+ * 
+ * Methoden: - elementsToJodaLocalTime: Erstellt ein org.joda.time.LocalDate Objekt aus übergebenen Strings für Tag, Monat und Jahr. -
+ * elementsToJodaLocalDate: Erstellt ein org.joda.time.LocalDate Objekt aus übergebenen Integern oder long Werten für Tag, Monat und Jahr. -
+ * elementsToTimeLocalTime: Erstellt ein java.time.LocalDate Objekt aus übergebenen Strings, Integern oder long Werten für Tag, Monat und Jahr. -
+ * singleStringToJodaLocalTime: Erstellt ein org.joda.time.LocalDate Objekt aus einem übergebenen Datum in einem String ohne Trennzeichen und einem
+ * Format. - singleStringToJavaLocalTime: Erstellt ein java.time.LocalDate Objekt aus einem übergebenen Datum in einem String ohne Trennzeichen und
+ * einem Format.
+ */
 public class DateUtils {
 
     /**
@@ -51,55 +62,76 @@ public class DateUtils {
         return new LocalDate((int) year, (int) month, (int) day);
     }
 
+    /**
+     * Erstellt ein java.time.LocalDate Objekt aus übergebenen Strings für Tag, Monat und Jahr.
+     *
+     * @param day Tag als String.
+     * @param month Monat als String.
+     * @param year Jahr als String.
+     * @return Ein java.time.LocalDate Objekt, das das angegebene Datum repräsentiert.
+     */
+    public static java.time.LocalDate elementsToTimeLocalTime(String day, String month, String year) {
 
-    public static LocalDate stringsToLocalDate(String day, String month, String year) {
-
-        return LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
-    }
-        
-
-    public static LocalDate intengersToLocalDate(int day, int month, int year) {
-
-        return LocalDate.of(year, month, day);
-    }
-
-    public static LocalDate longsToLocalDate(long day, long month, long year) {
-
-        return LocalDate.of((int) year, (int) month, (int) day);
+        String dateString = day + "-" + month + "-" + year;
+        java.time.format.DateTimeFormatter formatter = java.time.format.DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return java.time.LocalDate.parse(dateString, formatter);
     }
 
-    public static String localDateToString(String day, String month, String year, String format) {
+    /**
+     * Erstellt ein java.time.LocalDate Objekt aus übergebenen int-Werten für Tag, Monat und Jahr.
+     *
+     * @param day Der Tag als int-Wert.
+     * @param month Der Monat als int-Wert.
+     * @param year Das Jahr als int-Wert.
+     * @return Ein java.time.LocalDate Objekt, das das angegebene Datum repräsentiert.
+     */
+    public static java.time.LocalDate elementsToTimeLocalTime(int day, int month, int year) {
 
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(format);
-        return elementsToJodaLocalTime(day, month, year).format(outputFormatter);
+        return java.time.LocalDate.of(year, month, day);
     }
 
-    public static String localDateToIntengers(int day, int month, int year, String format) {
+    /**
+     * Erstellt ein java.time.LocalDate Objekt aus übergebenen long-Werten für Tag, Monat und Jahr.
+     *
+     * @param day Der Tag als long-Wert.
+     * @param month Der Monat als long-Wert.
+     * @param year Das Jahr als long-Wert.
+     * @return Ein java.time.LocalDate Objekt, das das angegebene Datum repräsentiert.
+     */
+    public static java.time.LocalDate elementsToTimeLocalTime(long day, long month, long year) {
 
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(format);
-        return elementsToJodaLocalDate(day, month, year).format(outputFormatter);
+        return java.time.LocalDate.of((int) year, (int) month, (int) day);
+    }
+
+    /**
+     * Erstellt ein org.joda.time.LocalDate Objekt aus einem übergebenen Datum in einem String ohne Trennzeichen und einem Format.
+     *
+     * @param date Der Datumstring ohne Trennzeichen.
+     * @param dateFormat Das Format des Datumstrings.
+     * @return Ein org.joda.time.LocalDate Objekt, das das angegebene Datum repräsentiert.
+     */
+    public static LocalDate singleStringToJodaLocalTime(String date, String dateFormat) {
+
+        DateTimeFormatter dateFormatter = DateTimeFormat.forPattern(dateFormat);
+        return LocalDate.parse(date, dateFormatter);
+    }
+
+    /**
+     * Erstellt ein org.joda.time.LocalDate Objekt aus einem übergebenen Datum in einem String ohne Trennzeichen und einem Format.
+     *
+     * @param dateString Der Datumstring ohne Trennzeichen.
+     * @param format Das Format des Datumstrings.
+     * @return Ein org.joda.time.LocalDate Objekt, das das angegebene Datum repräsentiert.
+     */
+    public static java.time.LocalDate singleStringToJavaLocalTime(String date, String dateFormat) {
+
+        java.time.format.DateTimeFormatter dateFormatter = java.time.format.DateTimeFormatter.ofPattern(dateFormat);
+        return java.time.LocalDate.parse(date, dateFormatter);
     }
 
     /*
-     * public static String localDateToLongs(long day, long month, long year, String format) {
+     * public static void main(String[] args) throws Exception {
      * 
-     * DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(format); return localDateFromLongs((int) day, (int) month, (int)
-     * year).format(outputFormatter); }
+     * System.out.println(singleStringToJodaLocalTime("20010201", "yyyyMMdd")); }
      */
-
-    public static LocalDate addDayToLocalDateNow(long day) {
-        return LocalDate.now().plusDays(day);
-    }
-
-    /*
-     * public static String addDay(LocalDate date, long day) {
-     * 
-     * String format = "dd.MM.yyyy"; return format.plusDays(day); }
-     */
-    public static void main(String[] args) throws Exception {
-
-        System.out.println(elementsToJodaLocalDate(27, 01, 2001));
-    }
-
-    }
-
+}
--- src/test/java/de/adesso/migsuite/utils/dateutils/test/DateUtilsTest.java
+++ src/test/java/de/adesso/migsuite/utils/dateutils/test/DateUtilsTest.java
@@ -12,39 +12,56 @@ public class DateUtilsTest {
 
     @Test
     public void testElementsToJodaLocalTime() throws ParseException {
-        LocalDate expectedDate = LocalDate.of(2001, 01, 27);
+        org.joda.time.LocalDate expectedDate = new org.joda.time.LocalDate(2001, 01, 27);
         assertEquals(DateUtils.elementsToJodaLocalTime("27", "01", "2001"), expectedDate);
     }
 
     @Test
-    public void testElementsToJodaLocalDate() throws ParseException {
-        LocalDate expectedDate = LocalDate.of(2001, 01, 27);
+    public void testElementsToJodaLocalDate1() throws ParseException {
+        org.joda.time.LocalDate expectedDate = new org.joda.time.LocalDate(2001, 01, 27);
         assertEquals(DateUtils.elementsToJodaLocalDate(27, 01, 2001), expectedDate);
     }
 
     @Test
-    public void testelementsToJodaLocalDate() throws ParseException {
-        LocalDate expectedDate = LocalDate.of(2001, 01, 27);
+    public void testElementsToJodaLocalDate() throws ParseException {
+        org.joda.time.LocalDate expectedDate = new org.joda.time.LocalDate(2001, 01, 27);
         assertEquals(DateUtils.elementsToJodaLocalDate(27l, 01l, 2001l), expectedDate);
     }
 
-    /*
-     * @Test public void stringsToLocalDate() throws ParseException { LocalDate expectedDate = LocalDate.of(2024, 05, 06);
-     * assertEquals(DateUtils.stringsToLocalDate("06", "05", "2024"), expectedDate); }
-     * 
-     * @Test public void intengersToLocalDate() throws ParseException { LocalDate expectedDate = LocalDate.of(2024, 05, 06);
-     * assertEquals(DateUtils.intengersToLocalDate(06, 05, 2024), expectedDate); }
-     * 
-     * @Test public void longsToLocalDate() throws ParseException { LocalDate expectedDate = LocalDate.of(2024, 05, 06);
-     * assertEquals(DateUtils.longsToLocalDate(06l, 05l, 2024l), expectedDate); }
-     * 
-     * @Test public void localDateToString() throws ParseException { String expectedDate = "06-06-2024";
-     * assertEquals(DateUtils.localDateToString("06", "06", "2024", "dd-MM-yyyy"), expectedDate); }
-     * 
-     * @Test public void localDateToIntengers() throws ParseException { String expectedDate = "06-06-2024";
-     * assertEquals(DateUtils.localDateToIntengers(06, 06, 2024, "dd-MM-yyyy"), expectedDate); }
-     * 
-     * @Test public void localDateToLongs() throws ParseException { String expectedDate = "06-06-2024";
-     * assertEquals(DateUtils.elementsToJodaLocalDate(06l, 06l, 2024l), expectedDate); }
-     */
+    @Test
+    public void testElementsToTimeLocalTime1() throws ParseException {
+        LocalDate expectedDate = LocalDate.of(2024, 05, 06);
+        assertEquals(DateUtils.elementsToTimeLocalTime("06", "05", "2024"), expectedDate);
+    }
+
+    @Test
+    public void testElementsToTimeLocalTime() throws ParseException {
+        LocalDate expectedDate = LocalDate.of(2024, 05, 06);
+        assertEquals(DateUtils.elementsToTimeLocalTime(06, 05, 2024), expectedDate);
+    }
+
+    @Test
+    public void longsToLocalDate() throws ParseException {
+        LocalDate expectedDate = LocalDate.of(2024, 05, 06);
+        assertEquals(DateUtils.longsToLocalDate(06l, 05l, 2024l), expectedDate);
+    }
+
+    @Test
+    public void localDateToString() throws ParseException {
+        String expectedDate = "06-06-2024";
+        assertEquals(DateUtils.localDateToString("06", "06", "2024", "dd-MM-yyyy"), expectedDate);
+    }
+
+    @Test
+    public void localDateToIntengers() throws ParseException {
+        String expectedDate = "06-06-2024";
+        assertEquals(DateUtils.localDateToIntengers(06, 06, 2024, "dd-MM-yyyy"), expectedDate);
+    }
+
+    @Test
+    public void localDateToLongs() throws ParseException {
+        String expectedDate = "2024-06-06";
+        assertEquals(DateUtils.elementsToJodaLocalDate(06l, 06l, 2024l), expectedDate);
+    }
+
 }
